342fb36e9b5908b2fcba6af681014648ce9c9532
Torsten Seemann <torsten.seemann@gmail.com>
2025-12-28 19:51:03 +1100

    Major CI change - Conda + BATS

---
+++ test/test.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bats
+
+setup() {
+  name="mlst"
+  bats_require_minimum_version 1.5.0
+  dir=$(dirname "$BATS_TEST_FILENAME")
+  cd "$dir"
+  exe="$dir/../bin/$name --quiet"
+  tab=$'\t'
+  SEPI="${tab}sepidermidis${tab}184${tab}"
+}
+
+@test "Script syntax check" {
+  run -0 perl -c "$dir/../bin/$name"
+}
+@test "Version" {
+  run -0 $exe --version
+  [[ "$output" =~ "$name" ]]
+}
+@test "Help" {
+  run -0 $exe --help
+  [[ "$output" =~ "threads" ]]
+}
+@test "Dep check" {
+  run -0 $exe --no-quiet --check
+  [[ "$output" =~ "OK" ]]
+}
+@test "No parameters" {
+  run $exe
+}
+@test "Bad option" {
+  run ! $exe --doesnotexist
+}
+@test "List schemes (short)" {
+  run -0 $exe --list
+  [[ "$output" =~ "saureus" ]]
+}
+@test "List schemes (long)" {
+  run -0 $exe --longlist
+  [[ "$output" =~ "saureus" ]]
+}
+@test "Passing a folder" {
+  run ! $exe $dir
+  [[ "$output" =~ "directory" ]]
+}
+@test "Null input" {
+  run $exe null.fa
+  [[ "$output" =~ "ERROR" ]]
+}
+@test "Empry input" {
+  run $exe --no-quiet empty.fa
+  [[ "$output" =~ "Sequence contains no data" ]]
+}
+@test "Gzipped FASTA" {
+  run -0 $exe example.fna.gz
+  [[ "$output" =~ "$SEPI" ]]  
+}
+@test "Bzipped FASTA" {
+  run -0 $exe novel.fasta.bz2
+  [[ "$output" =~ "leptospira_2" ]]  
+}
+@test "Accept STDIN" {
+  skip
+  run bats_pipe gzip -d -c test/example.fna.gz \| $exe /dev/stdin
+  [[ "$output" =~ $SEPI ]]  
+}
+@test "Two files in legacy mode " {
+  run -0 $exe example.fna.gz example.gbk.gz
+  [[ "$output" =~ $SEPI ]]  
+}
+@test "CSV output" {
+  run -0 $exe --csv example.fna.gz
+  [[ "$output" =~ ",184," ]]  
+}
+@test "JSON output" {
+  local outfile="${BATS_TMPDIR}/$name.json"
+  run -0 $exe --json "$outfile" example.fna.gz
+  [[ -r "$outfile" ]]
+  run -0 grep 'sequence_type' "$outfile"
+}
+@test "Custom label" {
+  run -0 $exe --label GDAYMATE example.fna.gz
+  [[ "$output" =~ "GDAYMATE" ]]
+}
+@test "Duplicate label" {
+  run ! $exe --label double_trouble example.gbk.gz example.fna.gz
+}
+@test "Save novel allele " {
+  local outfile="${BATS_TMPDIR}/$name.novel.fa"
+  run -0 $exe --novel "$outfile" novel.fasta.bz2
+  [[ -r "$outfile" ]]
+  run -0 grep 'mreA' "$outfile"
+}
+@test "Test issue 146" {
+  run -0 $exe issue146.fa
+  [[ "$output" =~ "purE(25)" ]]  
+}
+@test "Script: show_seqs" {
+  run -0 "$dir/../scripts/mlst-show_seqs" -s efaecium -t 111
+  [[ "$output" =~ ">atpA_2" ]]  
+}
